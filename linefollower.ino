b8d303bc8697a22a87c1a5039c5d97c43f10e4e2
Riconan <43110831+ADITYA0012345@users.noreply.github.com>
2020-01-30 19:03:16 +0530

    Add files via upload

---
+++ linefollower.ino
@@ -0,0 +1,52 @@
+#define LS 2      // left sensor
+#define RS 3      // right sensor
+int vel=180;
+#define LM1 5      // left motor
+#define LM2 6       // left motor
+#define RM1 10       // right motor
+#define RM2 11       // right motor
+
+void setup()
+{
+  pinMode(LS, INPUT);
+  pinMode(RS, INPUT);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(LS) && digitalRead(RS))     
+  {
+    analogWrite(LM1, vel);
+    analogWrite(LM2, LOW);
+    analogWrite(RM1, vel);
+    analogWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(LS)) && (digitalRead(RS)))     
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(digitalRead(LS) && !(digitalRead(RS)))    
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))    
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+}
